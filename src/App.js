cff289b6d1abf3f4b6bdb374e2e96546abb89d5e
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-02-03 23:12:12 +0100

    Add FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import './App.sass';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressBook } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faAddressBook)
+
 class App extends Component {
   render() {
     return (
