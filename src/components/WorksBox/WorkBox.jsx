94cd2f7fb1a8eb0e9b34ce6184e1a1980cb3f0db
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-06-07 02:13:59 +0200

    Finish the About section

--- src/components/WorksBox/WorkBox.jsx
+++ src/components/WorksBox/WorkBox.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLink } from '@fortawesome/free-solid-svg-icons'
 import './WorkBox.sass';
 
 const WorkBox = () => {
@@ -10,30 +8,17 @@ const WorkBox = () => {
   return (
     <div>
       <div className="side-borders"></div>
-      <div
-        style={{backgroundImage: 'url(' + bgImage + ')'}} 
-        className="workBox">
-        <div className="darkenBackground"></div>
-        <div className="work-title">
-          <h3>Work Title</h3>
-          <p>This is the subtitle</p>
-          <ul className="links-list">
-            <li>
-              <a title="Visit Live" href="https://website.com" target="_blank">
-                <FontAwesomeIcon icon={faLink} />
-              </a>
-            </li>
-            <li>
-              <a title="GitHub Repo" href="https://githubrepo.com" target="_blank">
-                <i className="fab fa-github"></i>
-              </a>
-            </li>
-            <li>
-              <Link title="Read more..." to="/works/1/wow-comparator">More...</Link>
-            </li>
-          </ul>
+      <Link to="/work/id/slug" className="workBox-link">
+        <div
+          style={{backgroundImage: 'url(' + bgImage + ')'}} 
+          className="workBox">
+          <div className="darkenBackground"></div>
+          <div className="work-title">
+            <h3>Work Title</h3>
+            <p>This is the subtitle</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
--- src/sections/index.jsx
+++ src/sections/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './Home/Home';
 import Works from './Works/Works';
+import About from './About/About';
 
 const Sections = () => {
   return (
     <div>
       <Home />
       <Works />
+      <About />
     </div>
   )
 }
